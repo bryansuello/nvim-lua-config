49bf938f5b8469063b54b2db6bb4da63ab92055e
suello.tech <suello.tech@gmail.com>
2022-08-19 03:40:34 +0800

    transparent bg, etc

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,4 +1,4 @@
-vim.cmd [[
+vim.cmd([[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
@@ -24,13 +24,27 @@ vim.cmd [[
     autocmd VimResized * tabdo wincmd = 
   augroup end
 
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-]]
+  " augroup _alpha
+  "   autocmd!
+  "   autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+  " augroup end
 
--- Autoformat
+  " set background transparent
+  augroup _colorscheme 
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
+augroup end
+  colorscheme gruvbox
+
+  
+
+ augroup _lsp
+   autocmd!
+   autocmd BufWritePre * lua vim.lsp.buf.formatting()
+ augroup end
+]])
+
+-- Autoformat on save
 -- augroup _lsp
 --   autocmd!
 --   autocmd BufWritePre * lua vim.lsp.buf.formatting()
