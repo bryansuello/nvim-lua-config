f6d390ed975d792d0c91359fd06793a72d18e536
brys <brys@weapon.localdomain>
2022-09-11 18:20:35 +0800

    monokai configured

--- init.lua
+++ init.lua
@@ -36,3 +36,22 @@ require "user.autocommands"
 --   overrides = {},
 -- })
 -- vim.cmd("colorscheme gruvbox")
+
+
+
+require("monokai").setup({
+  palette = require('monokai').pro,
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true, --  a combo of italics and ligatures make things unreadable
+  strikethrough = true,
+  --   invert_selection = false,
+  --   invert_signs = false,
+  --   invert_tabline = false,
+  --   invert_intend_guides = false,
+  --   inverse = true, -- invert background for search, diffs, statuslines and errors
+  --   contrast = "", -- can be "hard", "soft" or empty string
+  --   overrides = {},
+})
+vim.cmd("colorscheme monokai")
