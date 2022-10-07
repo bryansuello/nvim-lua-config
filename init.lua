b552f99fdc021524b0383673476a294365ef26ba
bryan suello <bryansuello@gmail.com>
2022-10-07 21:08:19 +0800

    installed nvcode theme plugin. switched

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ require "user.gitsigns"
 require "user.nvim-tree"
 require "user.bufferline"
 require "user.lualine"
---require "user.toggleterm"
+require "user.toggleterm"
 --require "user.project"
 require "user.impatient"
 require "user.indentline"
@@ -21,22 +21,22 @@ require "user.whichkey"
 require "user.autocommands"
 
 -- remove gruvbox italics before autocmd runs to remove background
-require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = true, --  a combo of italics and ligatures make things unreadable
-  italic_comments = true,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  overrides = {},
-})
-vim.cmd("colorscheme gruvbox")
+-- require("gruvbox").setup({
+--   undercurl = true,
+--   underline = true,
+--   bold = true,
+--   italic = true, --  a combo of italics and ligatures make things unreadable
+--   -- italic_comments = true,
+--   strikethrough = true,
+--   invert_selection = false,
+--   invert_signs = false,
+--   invert_tabline = false,
+--   invert_intend_guides = false,
+--   inverse = true, -- invert background for search, diffs, statuslines and errors
+--   contrast = "", -- can be "hard", "soft" or empty string
+--   overrides = {},
+-- })
+-- vim.cmd("colorscheme gruvbox")
 
 
 
@@ -106,3 +106,23 @@ vim.cmd("colorscheme gruvbox")
 --   -- },
 -- })
 -- vim.cmd("colorscheme vscode")
+
+
+
+-- nvcode
+require("nvim-treesitter").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true, --  a combo of italics and ligatures make things unreadable
+  italic_comments = true,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  overrides = {},
+})
+vim.cmd("colorscheme nvcode")
